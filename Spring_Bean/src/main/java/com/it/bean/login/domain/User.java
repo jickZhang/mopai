0eda9800aadfb9143dc4aa2b50c4f2597916139f
zhangchao <jick@192.168.31.93>
2019-03-07 08:53:42 +0800

    测试冲突

--- Spring_Bean/src/main/java/com/it/bean/login/domain/User.java
+++ Spring_Bean/src/main/java/com/it/bean/login/domain/User.java
@@ -5,7 +5,7 @@ public class User {
 	private String id; 
 	private String userName; //用户名  
 	private String loginName; //登陆名
-	private String age;
+	private String age;      //年龄
 
 	public String getUserName() {
 		return userName;
